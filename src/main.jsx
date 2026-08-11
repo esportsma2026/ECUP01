12c97bd5e44e9a56800e576f3b47b433ec90a759
esportsma2026 <esportsma2026@gmail.com>
2026-08-11 11:01:41 -0400

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { inject } from '@vercel/analytics'
 import { registerSW } from 'virtual:pwa-register'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 // Activate Vercel Analytics automatically
 inject()
@@ -14,5 +15,6 @@ registerSW({ immediate: true })
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
